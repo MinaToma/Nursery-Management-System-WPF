1d852bc6b664149b820fb19201a1981a140fa7a1
MinaToma <39103197+MinaToma@users.noreply.github.com>
2018-12-09 23:36:07 +0200

    added grid view to work on later

--- Nursery Management System WPF/Nursery Management System WPF/adminWindow.xaml.cs
+++ Nursery Management System WPF/Nursery Management System WPF/adminWindow.xaml.cs
@@ -30,5 +30,58 @@ namespace Nursery_Management_System_WPF
             logIn.Show();
             this.Close();
         }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            profileGrid.Visibility = Visibility.Visible;
+        }
+
+        public void resetGrid()
+        {
+            profileGrid.Visibility = Visibility.Hidden;
+        }
+
+        public void Sfill_info()
+        {
+            SQL mysql = new SQL();
+
+            GlobalVariables.globalAdmin.ToString();
+            GlobalVariables.globalStaff.ToString();
+            if (GlobalVariables.globalStaff.type == "Staff")
+            {
+                string staff_username = "select userName from User_Password where staffID like '" + (GlobalVariables.globalStaff.id).ToString() + "' ";
+                string staff_password = "select userPassword from User_Password where staffID  like'" + (GlobalVariables.globalStaff.id).ToString() + "' ";
+                string un = mysql.retrieveQuery(staff_username).ToString();
+                string pass = mysql.retrieveQuery(staff_password).ToString();
+                firstName.Text = GlobalVariables.globalStaff.firstName;
+                lastName.Text = GlobalVariables.globalStaff.lastName;
+                username.Text = un;
+                email.Text = GlobalVariables.globalStaff.email;
+                phoneNumber.Text = GlobalVariables.globalStaff.phoneNumber;
+                password.Password = pass;
+                ID.Text = (GlobalVariables.globalStaff.id).ToString();
+            }
+            else
+            {
+                string admin_username = "select userName from User_Password where staffID like '" + (GlobalVariables.globalAdmin.id).ToString() + "' ";
+                string admin_password = "select userPassword from User_Password where staffID  like'" + (GlobalVariables.globalAdmin.id).ToString() + "' ";
+                string un = mysql.retrieveQuery(admin_username).ToString();
+                string pass = mysql.retrieveQuery(admin_password).ToString();
+                firstName.Text = GlobalVariables.globalAdmin.firstName;
+                lastName.Text = GlobalVariables.globalAdmin.lastName;
+                username.Text = un;
+                email.Text = GlobalVariables.globalAdmin.email;
+                phoneNumber.Text = GlobalVariables.globalAdmin.phoneNumber;
+                password.Password = pass;
+                ID.Text = (GlobalVariables.globalAdmin.id).ToString();
+
+            }
+        }
     }
+ 
 }
